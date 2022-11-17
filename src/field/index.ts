290b891618b5240a6f57f37dba4af7a58c79eed3
iimm <liudichen@foxmail.com>
2022-11-17 18:23:33 +0800

    FormilyToggleButtonGroup

--- src/field/index.ts
+++ src/field/index.ts
@@ -1,6 +1,8 @@
+export * from './CheckboxGroup';
+export * from './RadioGroup';
 export * from './Input';
 export * from './Select';
 export * from './Switch';
-export * from './CheckboxGroup';
-export * from './RadioGroup';
-export * from './ToggleButtonGroup';
+
+export { ToggleButtonGroup, ToggleButtonGroupProps } from './ToggleButtonGroup';
+export { FormilyToggleButtonGroup } from './ToggleButtonGroup/formilyToggleButton';
