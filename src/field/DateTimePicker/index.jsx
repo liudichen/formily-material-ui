fefc920973ad6e8cb76287600ea1cd9723aec51e
iimm <liudichen@foxmail.com>
2022-11-18 13:35:50 +0800

    统一改造

--- src/field/DateTimePicker/index.jsx
+++ src/field/DateTimePicker/index.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import { useControllableValue } from 'ahooks';
 import { TextField } from '@mui/material';
 import { DateTimePicker as MuiDateTimePicker } from '@mui/x-date-pickers';
-import { observer, useField } from '@formily/react';
+import { observer } from '@formily/react';
+
+import { useFormilyFieldProps } from '../../hooks';
 
 export const DateTimePicker = observer((props) => {
+  const formilyFieldProps = useFormilyFieldProps(props, { label: true });
   const {
     // eslint-disable-next-line no-unused-vars
     value: valueProp, onChange: onChangeProp, defaultValue,
     size, fullWidth, color, variant, TextFieldSx,
     label, labelPosition,
     ...restProps
-  } = props;
-  const [ value, onChange ] = useControllableValue(props, { defaultValue: null });
-  const field = useField();
+  } = formilyFieldProps;
+  const [ value, onChange ] = useControllableValue(formilyFieldProps, { defaultValue: null });
 
   return (
     <MuiDateTimePicker
-      label={labelPosition === 'inner' ? (label ?? field?.title) : undefined}
+      label={labelPosition === 'inner' ? label : undefined}
       value={value || null}
       onChange={onChange}
       renderInput={(params) => <TextField {...params} size={size} fullWidth={fullWidth} color={color} variant={variant} sx={TextFieldSx}/>}
