97e922ae12660a236315dace9b6b101c208ee4a4
liudichen <liudichen@foxmail.com>
2022-11-18 20:32:17 +0800

    useFormilyFieldProps支持更多属性

--- src/hooks/useFormilyFieldProps/index.ts
+++ src/hooks/useFormilyFieldProps/index.ts
@@ -2,39 +2,106 @@ import { useField } from '@formily/react';
 import { isVoidField } from '@formily/core';
 
 import { ICommonProps } from '../../types';
+import React from 'react';
 
 interface IUseFormilyFieldProps {
+  /** @default undefined   */
   label?: boolean,
+  /** @default undefined   */
   required?: boolean,
+  /** @default undefined   */
   error?: boolean,
+  /** @default undefined   */
   tooltip?: boolean,
+  /** @default undefined   */
   options?: boolean,
+  /** @default undefined   */
+  display?: boolean,
+  /** @default true   */
+  defaultValue?: boolean,
+  /** @default true   */
+  disabled?: boolean,
+  /** @default true   */
+  readOnly?: boolean,
+  /** @default undefined   */
+  feedbackText?: boolean,
+  /** @default undefined   */
+  feedbackStatus?: boolean,
 }
 
+interface IProps extends ICommonProps {
+  label?: React.ReactNode,
+  tooltip?: React.ReactNode,
+  defaultValue?: any,
+  disabled?: boolean,
+  readOnly?: boolean,
+  error?: boolean,
+  required?: boolean,
+  options?: any,
+  display?: 'visible' | 'hidden' | 'none' | string,
+  feedbackStatus?: 'error' | 'warning' | 'success' | 'pending' | string,
+  feedbackText?: React.ReactNode,
+}
 
-export const useFormilyFieldProps = (props: ICommonProps, config: IUseFormilyFieldProps = {}) => {
+export const useFormilyFieldProps = (props: IProps, config: IUseFormilyFieldProps = {}) => {
   const field = useField();
-  if (!field || isVoidField(field) || !Object.keys(config || {}).length) return props;
+  if (!field) return props;
   const formatProps = {
     ...props,
   };
-  formatProps.defaultValue = props.defaultValue ?? field.initialValue;
-  formatProps.disabled = props.disabled ?? field.disabled;
-  formatProps.readOnly = props.readOnly ?? field.readOnly;
+  if (config.display) { formatProps.display = props.display ?? field.display; }
+  if (config?.label) { formatProps.label = props.label ?? field.title; }
+  if (config?.tooltip) { formatProps.tooltip = props?.tooltip ?? field.description; }
+  if (isVoidField(field)) {
+    return formatProps;
+  }
+  if (config?.defaultValue !== false) {
+    formatProps.defaultValue = props.defaultValue ?? field.initialValue;
+  }
+  if (config?.disabled !== false) {
+    formatProps.disabled = props.disabled ?? field.disabled;
+  }
+  if (config?.readOnly !== false) {
+    formatProps.readOnly = props.readOnly ?? field.readOnly;
+  }
   if (config?.error) {
     formatProps.error = props.error ?? field.selfInvalid;
   }
   if (config?.required) {
-    formatProps.required = props.required ?? field.required;
-  }
-  if (config?.label) {
-    formatProps.label = props.label ?? field.title;
-  }
-  if (config?.tooltip) {
-    formatProps.tooltip = props?.tooltip ?? field.description;
+    const takeRequired = () => {
+      if (field.required && field.pattern !== 'readPretty') {
+        return true;
+      }
+      if ('required' in props) {
+        return props.required;
+      }
+      return false;
+    };
+    formatProps.required = takeRequired();
   }
   if (config?.options) {
     formatProps.options = props?.options ?? field.dataSource;
+  }
+  if (config?.feedbackStatus) {
+    formatProps.feedbackStatus = props.feedbackStatus ?? (field.validating ? 'pending' : (field.decoratorProps.feedbackStatus || field.validateStatus));
+  }
+  if (config?.feedbackText) {
+    const takeMessage = () => {
+      if (field.validating) return;
+      if (props.feedbackText) return props.feedbackText;
+      const split = (messages: any[]) => {
+        return messages.reduce((buf, text, index) => {
+          if (!text) return buf;
+          return index < messages.length - 1
+            ? buf.concat([ text, ', ' ])
+            : buf.concat([ text ]);
+        }, []);
+      };
+      if (field.selfErrors.length) return split(field.selfErrors);
+      if (field.selfWarnings.length) return split(field.selfWarnings);
+      if (field.selfSuccesses.length) return split(field.selfSuccesses);
+    };
+    formatProps.feedbackText = takeMessage();
   }
   return formatProps;
 };
