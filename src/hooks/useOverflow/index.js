58861e1dbfc769bd0288a12ea5ed2937e4b8c114
iimm <liudichen@foxmail.com>
2022-11-18 08:21:52 +0800

    useSize内部监控

--- src/hooks/useOverflow/index.js
+++ src/hooks/useOverflow/index.js
@@ -1,21 +1,22 @@
 import { useEffect, useRef } from 'react';
-import { useSafeState } from 'ahooks';
+import { useSafeState, useSize } from 'ahooks';
 
-export const useOverflow = (deps = []) => {
+export const useOverflow = () => {
   const [ overflow, setOverflow ] = useSafeState(false);
   const containerRef = useRef();
   const contentRef = useRef();
+  const containerSize = useSize(containerRef);
+  const containerWidth = containerSize?.width;
   useEffect(() => {
     requestAnimationFrame(() => {
       const contentWidth = contentRef.current?.getBoundingClientRect()?.width;
-      const containerWidth = containerRef.current?.getBoundingClientRect()?.width;
       if (contentWidth && containerWidth && containerWidth < contentWidth) {
         if (!overflow) setOverflow(true);
       } else {
         if (overflow) setOverflow(false);
       }
     });
-  }, [ ...deps ]);
+  }, [ containerWidth ]);
 
   return {
     overflow,
