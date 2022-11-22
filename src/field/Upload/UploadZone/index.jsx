5e6e5feea5417e75c28274c49b42a058a8063054
iimm <liudichen@foxmail.com>
2022-11-22 14:24:42 +0800

    按照文档，不直接将ref传递给input

--- src/field/Upload/UploadZone/index.jsx
+++ src/field/Upload/UploadZone/index.jsx
@@ -3,10 +3,11 @@ import { useDropzone } from 'react-dropzone';
 
 export const UploadZone = React.forwardRef((props, ref) => {
   const { children, ...restProps } = props;
-  const { getInputProps, getRootProps } = useDropzone({ ...restProps });
+  const { getInputProps, getRootProps, inputRef, open, rootRef } = useDropzone({ ...restProps });
+  React.useImperativeHandle(ref, () => ({ inputRef, rootRef, open }));
   return (
     <div {...getRootProps()}>
-      <input ref={ref} {...getInputProps()} />
+      <input {...getInputProps()} />
       {children}
     </div>
   );
