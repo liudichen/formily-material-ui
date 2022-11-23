cd8c016f6383d8f2193b264db4b8fd0e0b5f1265
iimm <liudichen@foxmail.com>
2022-11-23 11:13:00 +0800

    增加

--- src/utils/constant/index.ts
+++ src/utils/constant/index.ts
@@ -4,8 +4,12 @@ export const prefixCls = 'iimm';
 export const iconfontCssPrefix = 'iconfont';
 
 /** 适用于useFormilyFieldProps时FormItem的config */
-export const UseFormilyFieldPropsFormItemConfig = { tooltip: true, label: true, error: true, required: true, readOnly: false, disabled: false, feedbackStatus: true, feedbackText: true, defaultValue: false,
+export const UseFormilyFieldPropsFormItemConfig = {
+  tooltip: true, label: true, error: true, required: true,
+  readOnly: false, disabled: false, defaultValue: false,
+  feedbackStatus: true, feedbackText: true,
   labelPosition: true, labelAlign: true, labelWidth: true, labelWrap: true, wrapperAlign: true, wrapperWidth: true, wrapperWrap: true, fullWidth: true, colon: true, tooltipIcon: true, tooltipLayout: true, showFeedback: true, feedbackLayout: true,
+  withFormItem: false,
 };
 
 /** 适用于useFormilyFieldProps时formfield的基础通用config */
@@ -13,4 +17,5 @@ export const UseFormilyFieldPropsFormFieldBaseConfig = {
   readOnly: true,
   disabled: true,
   defaultValue: true,
+  withFormItem: true,
 };
