a27de12db51653bc65e5b62c972f4961dfdd91a1
iimm <liudichen@foxmail.com>
2022-11-23 08:47:57 +0800

    FormItemBase

---
+++ src/layout/FormItem/Base.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { FormItemProps } from './index';
+
+export declare const FormItemBase: React.FC<React.PropsWithChildren<FormItemProps>>;
--- src/layout/index.ts
+++ src/layout/index.ts
@@ -1,2 +1,3 @@
 export * from './FormItem';
 export * from './FormLayout';
+export * from './FormItem/Base';
