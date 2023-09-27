e05e2a6b75f0be23fe8d900e9d8b051c9c39e933
iimm <liudichen@foxmail.com>
2023-09-27 09:31:06 +0800

    1.6.11 StepsForm子StepForm改为不显示的display='none'

--- src/formily/StepsForm/index.tsx
+++ src/formily/StepsForm/index.tsx
@@ -1,46 +1,75 @@
-import { useEffect, useImperativeHandle, Children, cloneElement } from 'react';
-import type { ComponentType, RefObject, ReactNode } from 'react';
-import { useCreation, useMemoizedFn, useSafeState } from 'ahooks';
-import { createForm, type IFormProps, type Form } from '@formily/core';
-import { toJS } from '@formily/reactive';
-import { FormProvider, ObjectField } from '@formily/react';
-import { Box, Step, StepContent, StepIcon, StepLabel, Stepper, type StepperProps, type StepContentProps, type StepIconProps } from '@mui/material';
+import { useEffect, useImperativeHandle, Children, cloneElement } from "react";
+import type { ComponentType, RefObject, ReactNode } from "react";
+import { useCreation, useMemoizedFn, useSafeState } from "ahooks";
+import { createForm, type IFormProps, type Form } from "@formily/core";
+import { toJS } from "@formily/reactive";
+import { FormProvider, ObjectField } from "@formily/react";
+import {
+  Box,
+  Step,
+  StepContent,
+  StepIcon,
+  StepLabel,
+  Stepper,
+  type StepperProps,
+  type StepContentProps,
+  type StepIconProps,
+} from "@mui/material";
 
-import { StepForm, type StepFormProps } from './StepForm';
-import DefaultCompleteRender, { type ResultRenderProps as StepsFormResultRenderProps } from './DefaultCompleteRender';
+import { StepForm, type StepFormProps } from "./StepForm";
+import DefaultCompleteRender, { type ResultRenderProps as StepsFormResultRenderProps } from "./DefaultCompleteRender";
 
-interface StepsFormProps extends StepperProps, Omit<StepsFormResultRenderProps, 'values' | 'form' | 'handleStepChange'> {
-  form?: Form,
-  direction?: 'horizontal'| 'vertical',
-  labelPlacement?: 'horizontal'| 'vertical',
-  onFinish?: ((values?: object, allValues?: object) => void | boolean) | ((values?: object, allValues?: object) => Promise<void | boolean>),
+interface StepsFormProps
+  extends StepperProps,
+    Omit<StepsFormResultRenderProps, "values" | "form" | "handleStepChange"> {
+  form?: Form;
+  direction?: "horizontal" | "vertical";
+  labelPlacement?: "horizontal" | "vertical";
+  onFinish?:
+    | ((values?: object, allValues?: object) => void | boolean)
+    | ((values?: object, allValues?: object) => Promise<void | boolean>);
   /** 传递给createForm的参数
    * @default {validateFirst:true}
-  */
-  createFormOptions?: IFormProps,
+   */
+  createFormOptions?: IFormProps;
 
-  ResultRender?: ComponentType<StepsFormResultRenderProps>,
+  ResultRender?: ComponentType<StepsFormResultRenderProps>;
 
-  stepContentProps?: StepContentProps
+  stepContentProps?: StepContentProps;
   /** 重建form实例的depend项 */
-  depend?: any,
+  depend?: any;
   /** 获取form实例 */
-  formRef?: RefObject<Form>
-  showReset?: boolean
-  children?: ReactNode
+  formRef?: RefObject<Form>;
+  showReset?: boolean;
+  children?: ReactNode;
 }
 
 const StepsForm = (props: StepsFormProps) => {
   const {
     children,
-    onFinish, createFormOptions,
-    ResultRender = DefaultCompleteRender, resultTitle, resultSubTitle, showResultReset, resultActions, resultResetText, resultResetProps, onResultReset, resultContent,
+    onFinish,
+    createFormOptions,
+    ResultRender = DefaultCompleteRender,
+    resultTitle,
+    resultSubTitle,
+    showResultReset,
+    resultActions,
+    resultResetText,
+    resultResetProps,
+    onResultReset,
+    resultContent,
     stepContentProps,
-    direction = 'horizontal', orientation, alternativeLabel, labelPlacement = 'horizontal', depend,
-    formRef, form: formProp, showReset: showResetParent,
+    direction = "horizontal",
+    orientation,
+    alternativeLabel,
+    labelPlacement = "horizontal",
+    depend,
+    formRef,
+    form: formProp,
+    showReset: showResetParent,
     ...restProps
   } = props;
-  const [ stepsCount, setStepCount ] = useSafeState(() => Children.count(children));
+  const [stepsCount, setStepCount] = useSafeState(() => Children.count(children));
   const getCount = useMemoizedFn(() => {
     let count = 0;
     Children.map(children, (child) => {
@@ -53,23 +82,26 @@ const StepsForm = (props: StepsFormProps) => {
   const count = getCount();
   useEffect(() => {
     setStepCount(count);
-  }, [ count ]);
-  const [ activeStep, setActiveStep ] = useSafeState(0);
-  const form = useCreation(() => formProp || createForm(createFormOptions || { validateFirst: true }), [ depend, createFormOptions, formProp ]);
-  useImperativeHandle(formRef, () => form, [ form ]);
+  }, [count]);
+  const [activeStep, setActiveStep] = useSafeState(0);
+  const form = useCreation(
+    () => formProp || createForm(createFormOptions || { validateFirst: true }),
+    [depend, createFormOptions, formProp]
+  );
+  useImperativeHandle(formRef, () => form, [form]);
   const handleStepChange = useMemoizedFn((step) => {
-    if (typeof step === 'number') {
+    if (typeof step === "number") {
       setActiveStep(step);
     } else {
-      if (step === 'next') {
+      if (step === "next") {
         setActiveStep((s) => s + 1);
-      } else if (step === 'previous') {
-        setActiveStep((s) => (s - 1) || 0);
+      } else if (step === "previous") {
+        setActiveStep((s) => s - 1 || 0);
       }
     }
   });
   const handleReset = useMemoizedFn(() => {
-    form?.reset('*');
+    form?.reset("*");
     setActiveStep(0);
   });
   return (
@@ -78,13 +110,23 @@ const StepsForm = (props: StepsFormProps) => {
         <Stepper
           activeStep={activeStep}
           orientation={direction ?? orientation}
-          alternativeLabel={alternativeLabel ?? (labelPlacement === 'vertical')}
+          alternativeLabel={alternativeLabel ?? labelPlacement === "vertical"}
           {...restProps}
         >
           {Children.map(children, (child, index) => {
-            if (!child) { return null; }
+            if (!child) {
+              return null;
+            }
             // @ts-ignore
-            const { title, subTitle, icon = StepIcon, onFinish: onFinishProp, name, showReset: showResetProp, onReset } = child?.props || {};
+            const {
+              title,
+              subTitle,
+              icon = StepIcon,
+              onFinish: onFinishProp,
+              name,
+              showReset: showResetProp,
+              onReset,
+            } = child?.props || {};
             const showReset = showResetProp ?? showResetParent;
             const overwriteProps: Partial<StepFormProps> = {
               handleStepChange,
@@ -107,15 +149,11 @@ const StepsForm = (props: StepsFormProps) => {
                 <StepLabel optional={subTitle} StepIconComponent={icon}>
                   {title}
                 </StepLabel>
-                {(direction ?? orientation) === 'vertical' && (
+                {(direction ?? orientation) === "vertical" && (
                   <StepContent {...(stepContentProps || {})}>
                     <ObjectField name={name ?? index}>
                       {(field) => {
-                        return (
-                          <>
-                            {cloneElement(child as any, { ...overwriteProps, field })}
-                          </>
-                        );
+                        return <>{cloneElement(child as any, { ...overwriteProps, field })}</>;
                       }}
                     </ObjectField>
                   </StepContent>
@@ -124,9 +162,11 @@ const StepsForm = (props: StepsFormProps) => {
             );
           })}
         </Stepper>
-        {(direction ?? orientation) !== 'vertical' && (
+        {(direction ?? orientation) !== "vertical" &&
           Children.map(children, (child, index) => {
-            if (!child || index !== activeStep) { return null; }
+            if (!child) {
+              return null;
+            }
             // @ts-ignore
             const { onFinish: onFinishProp, name, showReset: showResetProp, onReset } = child?.props || {};
             const showReset = showResetProp ?? showResetParent;
@@ -150,15 +190,14 @@ const StepsForm = (props: StepsFormProps) => {
               <ObjectField name={name ?? index}>
                 {(field) => {
                   return (
-                    <Box>
+                    <Box sx={index !== activeStep ? { display: "none" } : {}}>
                       {cloneElement(child as any, { ...overwriteProps, field })}
                     </Box>
                   );
                 }}
               </ObjectField>
             );
-          })
-        )}
+          })}
         {activeStep === stepsCount && stepsCount !== 0 && (
           <ResultRender
             handleStepChange={handleStepChange}
@@ -180,15 +219,9 @@ const StepsForm = (props: StepsFormProps) => {
 };
 StepsForm.defaultProps = {
   showResultReset: true,
-  resultResetText: '返回',
+  resultResetText: "返回",
 };
 
 StepsForm.StepForm = StepForm;
 
-export {
-  StepsForm,
-  type StepsFormProps,
-  type StepFormProps,
-  type StepIconProps,
-  type StepsFormResultRenderProps,
-};
+export { StepsForm, type StepsFormProps, type StepFormProps, type StepIconProps, type StepsFormResultRenderProps };
