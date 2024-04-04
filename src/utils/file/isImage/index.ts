a14b28bd6a825c53220c430f4a19b98369baae4e
liudichen <liudichen@foxmail.com>
2024-04-04 20:08:02 +0800

    1.13.0

--- src/utils/file/isImage/index.ts
+++ src/utils/file/isImage/index.ts
@@ -1,17 +1,14 @@
-import { isImageMimeType, getFileExtensionFromUrl } from '@iimm/shared';
+import { isImageMimeType, getFileExtensionFromUrl } from "@iimm/shared";
 
-import { IUploadedFile } from '../../../types';
+import { type IUploadedFile } from "../../../types";
 
 export const isImage = (file: IUploadedFile) => {
   if (file.type && !file.thumbUrl && !file.url) {
     return isImageMimeType(file.type);
   }
-  const url = file.thumbUrl || file.url || '';
+  const url = file.thumbUrl || file.url || "";
   const extension = getFileExtensionFromUrl(url);
-  if (
-    /^data:image\//.test(url) ||
-    /(webp|svg|png|gif|jpg|jpeg|jfif|bmp|dpg|ico)$/i.test(extension)
-  ) {
+  if (/^data:image\//.test(url) || /(webp|svg|png|gif|jpg|jpeg|jfif|bmp|dpg|ico)$/i.test(extension)) {
     return true;
   }
   if (/^data:/.test(url)) {
